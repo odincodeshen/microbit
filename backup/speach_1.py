b8163314fc6f2d872cb12b3285ed952651081b17
Odin Shen Coder <odin.code.shen@gmail.com>
2016-11-24 11:39:53 +0800

    Revert "Init version"
    This reverts commit e9bbe21fb3b6cdbcc4c52d5009ad7422d86f7a32.

--- backup/speach_1.py
+++
@@ -1,5 +0,0 @@
-import speech
-
-speech.say("Hello, World")
-
-speech.say("I am Odin from Taipei", speed=120, pitch=100, throat=200, mouth=200)
--- backup/storage_1.py
+++
@@ -1,3 +0,0 @@
-with open('story.txt') as my_file:
-    content = my_file.read()
-print(content)
